fc97f7e33eb70dd0db99628205c5a5473b6bcbf5
swamini10 <swaminibhandare10@gmail.com>
2025-09-23 22:27:46 +0530

    Swapping_number

---
+++ www.project.demo/src/demo/Swapping_number{.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class Swapping_number{
+ public static void main(String[] args) {
+//swap by using third variable 
+	 
+//	  int a=10;
+//	  int b=20;
+//	  int t = a;
+//	   a=b;
+//	   b=t;
+//	   System.out.println("a"+a);
+//	   System.out.println("b"+b);
+	 
+//   swapping without using 3rd variable	 
+	 int a=9;
+	 int b=8; 
+	 b=a+b;
+	 a=b-a;
+	 b=b-a;	 
+   System.out.println("a:"+a);
+   System.out.println("b:"+b);
+}
+ }
